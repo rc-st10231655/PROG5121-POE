c43141cf195dda69d77f09a6d1acb3d25ec5d726
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-09-24 10:15:12 +0200

    Setup project

---
+++ src/main/java/com/mycompany/prog5121_poe/PROG5121_POE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121_poe;
+
+/**
+ *
+ * @author Malope
+ */
+public class PROG5121_POE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
