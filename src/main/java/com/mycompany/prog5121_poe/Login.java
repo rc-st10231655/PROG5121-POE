c64306634fbb4fa67d8fb696340b2c7aff60696d
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-09-30 12:04:21 +0200

    Add string builder instance in registerUser method Update variable names Update entire login comments

--- src/main/java/com/mycompany/prog5121_poe/Login.java
+++ src/main/java/com/mycompany/prog5121_poe/Login.java
@@ -12,49 +12,55 @@ import java.util.regex.Pattern;
  * @author ST10231655
  */
 public class Login {
-    // Declarations
-    private String firstName; // First name string
-    private String lastName; // Last name string
-    private String username; // Username string
-    private String password; // Password string
+    /*
+    Declarations
+     */
+    private String userFirstName; // User first name string
+    private String userLastName; // User last name string
+    private String userUsername; // User username string
+    private String userPassword; // User Password string
 
     // Constructor for login
-    public Login(String firstName, String lastName) {
-        this.firstName = firstName; // Assign firstname for login
-        this.lastName = lastName; // Assign las
+    public Login(String enteredFirstName, String enteredLastName) {
+        // Assign user first name as entered first name
+        this.userFirstName = enteredFirstName;
+        // Assign user last name as entered last name
+        this.userLastName = enteredLastName;
     }
 
     // Method to check username
-    public boolean checkUserName(String username) {
-        // Check if username has underscore and 5 characters
-        if (username.contains("_") && username.length() <= 5) {
-            // Assign username for login
-            this.username = username;
+    public boolean checkUserName(String enteredUsername) {
+        // Check if username has an underscore and 5 characters maximum
+        if (enteredUsername.contains("_") && enteredUsername.length() <= 5) {
+            // Assign entered username as user username
+            this.userUsername = enteredUsername;
             return true;
         }
         return false;
     }
 
     // Method to check password
-    public boolean checkPasswordComplexity(String password) {
-        // Check if password has 8 characters
-        if (password.length() >= 8) {
-            // Checks for any uppercase letter from A to Z
-            Pattern capitalLetterPattern = Pattern.compile("[A-Z]");
-            // Checks for any digit from 0 to 9
-            Pattern numberPattern = Pattern.compile("[0-9]");
-            // Checks for any character not a letter or digit
-            Pattern specialCharacterPattern = Pattern.compile("[^a-zA-Z0-9]");
+    public boolean checkPasswordComplexity(String enteredPassword) {
+        // Check if password has 8 characters minimum
+        if (enteredPassword.length() >= 8) {
+            // Declare capital letter pattern instance
+            Pattern capLetterPatt = Pattern.compile("[A-Z]"); // Checks for any uppercase letter from A to Z
+            // Declare number pattern object
+            Pattern numPatt = Pattern.compile("[0-9]"); // Checks for any digit from 0 to 9
+            // Declare special character pattern object
+            Pattern specCharPatt = Pattern.compile("[^a-zA-Z0-9]"); // Checks for any character not a letter or digit
 
-            // Matches capital letter pattern with password
-            Matcher hasCapitalLetter = capitalLetterPattern.matcher(password);
-            // Matches number pattern with password
-            Matcher hasNumber = numberPattern.matcher(password);
-            // Matches special character pattern with password
-            Matcher hasSpecialCharacter = specialCharacterPattern.matcher(password);
+            // Declare has capital letter matcher object
+            Matcher hasCapLetter = capLetterPatt.matcher(enteredPassword); // Matches capital letter pattern with password
+            // Declare has number matcher object
+            Matcher hasNum = numPatt.matcher(enteredPassword); // Matches number pattern with password
+            // Declare has capital letter matcher object
+            Matcher hasSpecChar = specCharPatt.matcher(enteredPassword); // Matches special character pattern with password
 
-            if (hasCapitalLetter.find() && hasNumber.find() && hasSpecialCharacter.find()) {
-                this.password = password; // Assign password for login
+            // Checks if password has capital letter, number and special character
+            if (hasCapLetter.find() && hasNum.find() && hasSpecChar.find()) {
+                // Assign entered password as user password
+                this.userPassword = enteredPassword;
                 return true;
             }
         }
@@ -62,30 +68,42 @@ public class Login {
     }
 
     // Method to register user and return message
-    public String registerUser(String username, String password) {
-        if (!checkUserName(username)) {
-            // Return username incorrectly formatted message
-            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length";
+    public String registerUser(String enteredUsername, String enteredPassword) {
+        // Declare registration message string builder
+        StringBuilder registrationMessage = new StringBuilder();
+
+        // Check if username is correctly formatted
+        if (!checkUserName(enteredUsername)) {
+            // Append username incorrectly formatted message to registration messages
+            registrationMessage.append("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.\n");
         }
-        if (!checkPasswordComplexity(password)) {
-            // Return password incorrectly formatted message
-            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        // Check if password is correctly formatted
+        if (!checkPasswordComplexity(enteredPassword)) {
+            // Append Password incorrectly formatted message to registration messages
+            registrationMessage.append("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and a special character.");
         }
-        // Return user registered successfully message
+
+        // Check for registration message appended
+        if (registrationMessage.length() > 0) {
+            // Return the registration message as string
+            return registrationMessage.toString();
+        }
+
+        // Return successful registration message
         return "User registered successfully";
     }
 
     // Method to login user
-    public boolean loginUser(String username, String password) {
-        // Check if entered username and password are same as username and password
-        return username.equals(this.username) && password.equals(this.password);
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+        // Check if entered username and password are same as user username and password
+        return enteredUsername.equals(this.userUsername) && enteredPassword.equals(this.userPassword);
     }
 
     // Method to return login status message
-    public String returnLoginStatus(String username, String password) {
-        if (loginUser(username, password)) {
+    public String returnLoginStatus(String enteredUsername, String enteredPassword) {
+        if (loginUser(enteredUsername, enteredPassword)) {
             // Return successful login message
-            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+            return "Welcome " + userFirstName + ", " + userLastName + " it is great to see you again.";
         } else {
             // Return failed login message
             return "Username or password incorrect, please try again";
