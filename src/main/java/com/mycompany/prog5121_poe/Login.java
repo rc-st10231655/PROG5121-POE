3cf031e55f0d78fc7285c24b07f047d2b11fe011
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-09-30 07:40:43 +0200

    Add checkPasswordComplexity method and comments

--- src/main/java/com/mycompany/prog5121_poe/Login.java
+++ src/main/java/com/mycompany/prog5121_poe/Login.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.prog5121_poe;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author ST10231655
@@ -20,10 +23,37 @@ public class Login {
 
     // Method to check username
     public boolean checkUserName(String username) {
-        // Check if username has underscore and 5 11characters
+        // Check if username has underscore and 5 characters
         if (username.contains("_") && username.length() <= 5) {
-            this.username = username; // Assign username for login
+            // Assign username for login
+            this.username = username;
             return true;
+        }
+        return false;
+    }
+
+    // Method to check password
+    public boolean checkPasswordComplexity(String password) {
+        // Check if password has 8 characters
+        if (password.length() >= 8) {
+            // Checks for any uppercase letter from A to Z
+            Pattern capitalLetterPattern = Pattern.compile("[A-Z]");
+            // Checks for any digit from 0 to 9
+            Pattern numberPattern = Pattern.compile("[0-9]");
+            // Checks for any character not a letter or digit
+            Pattern specialCharacterPattern = Pattern.compile("[^a-zA-Z0-9]");
+
+            // Matches capital letter pattern with password
+            Matcher hasCapitalLetter = capitalLetterPattern.matcher(password);
+            // Matches number pattern with password
+            Matcher hasNumber = numberPattern.matcher(password);
+            // Matches special character pattern with password
+            Matcher hasSpecialCharacter = specialCharacterPattern.matcher(password);
+
+            if (hasCapitalLetter.find() && hasNumber.find() && hasSpecialCharacter.find()) {
+                this.password = password; // Assign password for login
+                return true;
+            }
         }
         return false;
     }
