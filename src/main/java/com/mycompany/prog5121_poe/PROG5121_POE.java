4698e4f0bd1a5dd5085e61446fd4a4d55db4a429
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-09-30 09:25:51 +0200

    Update main method and add comments

--- src/main/java/com/mycompany/prog5121_poe/PROG5121_POE.java
+++ src/main/java/com/mycompany/prog5121_poe/PROG5121_POE.java
@@ -4,13 +4,61 @@
 
 package com.mycompany.prog5121_poe;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ST10231655
  */
 public class PROG5121_POE {
 
+    // Main method
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Declare scanner object instance
+        Scanner scannerObj = new Scanner(System.in);
+
+        System.out.println("------ Register ------");
+        // Prompt user to enter first name
+        System.out.print("Enter first name: ");
+        // User enters first name
+        String firstName = scannerObj.nextLine();
+        // Prompt user to enter last name
+        System.out.print("Enter last name: ");
+        // User enters last name
+        String lastName = scannerObj.nextLine();
+
+        // Declare login object instance
+        Login newUser = new Login(firstName, lastName);
+
+        // Prompt user to enter username
+        System.out.print("Enter username: ");
+        // User enters username
+        String username = scannerObj.nextLine();
+        // Prompt user to enter password
+        System.out.print("Enter password: ");
+        // User enters password
+        String password = scannerObj.nextLine();
+
+        // Assign registration status
+        String registerStatus = newUser.registerUser(username, password);
+        // Display registration status message
+        System.out.println(registerStatus);
+
+        System.out.println("------ Login ------");
+        // Prompt user to enter username
+        System.out.print("Enter username: ");
+        // User enters last name
+        String enteredUsername = scannerObj.nextLine();
+        // Prompt user to enter password
+        System.out.print("Enter password: ");
+        String enteredPassword = scannerObj.nextLine();// User enters last name
+
+        // Assign login status message
+        String loginStatus = newUser.returnLoginStatus(enteredUsername, enteredPassword);
+        // Display login status message
+        System.out.println(loginStatus);
+
+        // Close the scanner object instance
+        scannerObj.close();
     }
 }
