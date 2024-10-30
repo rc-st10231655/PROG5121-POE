c49f72e1b102dbed56411fb2fb9de2675ae6e7da
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-10-30 19:48:53 +0200

    Added task class file

---
+++ src/main/java/com/mycompany/prog5121_poe/Task.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prog5121_poe;
+
+/**
+ *
+ * @author ST10231655
+ */
+public class Task {
+
+}
