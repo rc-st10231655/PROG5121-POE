0ceacb479af35c3b4680212fb37674fdef97cb15
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-10-30 19:56:19 +0200

    Added task test class file

---
+++ src/test/java/com/mycompany/prog5121_poe/TaskTest.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.prog5121_poe;
+
+/**
+ *
+ * @author ST10231655
+ */
+// Task test class
+public class TaskTest {
+
+}
