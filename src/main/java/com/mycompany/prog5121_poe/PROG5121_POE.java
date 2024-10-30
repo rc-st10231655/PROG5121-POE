87ca9d26e1968daa5b885c89277568279aae1e8b
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-10-30 19:44:25 +0200

    Updated console with JOptionPane

--- src/main/java/com/mycompany/prog5121_poe/PROG5121_POE.java
+++ src/main/java/com/mycompany/prog5121_poe/PROG5121_POE.java
@@ -4,7 +4,7 @@
 
 package com.mycompany.prog5121_poe;
 
-import java.util.Scanner;
+import javax.swing.*;
 
 /**
  *
@@ -12,53 +12,31 @@ import java.util.Scanner;
  */
 public class PROG5121_POE {
 
-    // Main method
     public static void main(String[] args) {
-        // Declare scanner object instance
-        Scanner scannerObj = new Scanner(System.in);
-
-        System.out.println("------ Register ------");
         // Prompt user to enter first name
-        System.out.print("Enter first name: ");
-        // User enters first name
-        String firstName = scannerObj.nextLine();
+        String firstName = JOptionPane.showInputDialog("Enter first name:");
         // Prompt user to enter last name
-        System.out.print("Enter last name: ");
-        // User enters last name
-        String lastName = scannerObj.nextLine();
+        String lastName = JOptionPane.showInputDialog("Enter last name:");
 
-        // Declare login object instance
+        // Declare login instance
         Login newUser = new Login(firstName, lastName);
 
         // Prompt user to enter username
-        System.out.print("Enter username: ");
-        // User enters username
-        String username = scannerObj.nextLine();
+        String username = JOptionPane.showInputDialog("Enter username:");
         // Prompt user to enter password
-        System.out.print("Enter password: ");
-        // User enters password
-        String password = scannerObj.nextLine();
+        String password = JOptionPane.showInputDialog("Enter password:");
 
-        // Assign registration status
         String registerStatus = newUser.registerUser(username, password);
-        // Display registration status message
-        System.out.println(registerStatus);
+        // Display registration status
+        JOptionPane.showMessageDialog(null, registerStatus);
 
-        System.out.println("------ Login ------");
         // Prompt user to enter username
-        System.out.print("Enter username: ");
-        // User enters last name
-        String enteredUsername = scannerObj.nextLine();
+        String enteredUsername = JOptionPane.showInputDialog("Enter username:");
         // Prompt user to enter password
-        System.out.print("Enter password: ");
-        String enteredPassword = scannerObj.nextLine();// User enters last name
+        String enteredPassword = JOptionPane.showInputDialog("Enter password:");
 
-        // Assign login status message
         String loginStatus = newUser.returnLoginStatus(enteredUsername, enteredPassword);
-        // Display login status message
-        System.out.println(loginStatus);
-
-        // Close the scanner object instance
-        scannerObj.close();
+        // Display login status
+        JOptionPane.showMessageDialog(null, loginStatus);
     }
 }
