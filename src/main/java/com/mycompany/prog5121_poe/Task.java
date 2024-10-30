e745bad30946dfc5236c7ceb2b8aedf9bf10b7c6
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-10-30 19:51:47 +0200

    Added task constructor, check task description method, create task ID method, print task details method, return total hours method

--- src/main/java/com/mycompany/prog5121_poe/Task.java
+++ src/main/java/com/mycompany/prog5121_poe/Task.java
@@ -9,5 +9,56 @@ package com.mycompany.prog5121_poe;
  * @author ST10231655
  */
 public class Task {
+    private String taskName; // Task name string
+    private int taskNum; // Task number integer
+    private String taskDescription; // Task description string
+    private String devDetails; // Task developer details string
+    private int taskDuration; // Task duration integer
+    private String taskID; // Task ID string
+    private String taskStatus; // Task status string
 
+    // Task constructor
+    public Task(String taskName, int taskNum, String taskDescription, String devDetails, int taskDuration, String taskStatus) {
+        this.taskName = taskName;
+        this.taskNum = taskNum;
+        this.taskDescription = taskDescription;
+        this.devDetails = devDetails;
+        this.taskDuration = taskDuration;
+        this.taskID = createTaskID();
+        this.taskStatus = taskStatus;
+    }
+
+    // Method to check task description length
+    public boolean checkTaskDescription() {
+        // Return task description if length is less than 50
+        return this.taskDescription.length() <= 50;
+    }
+
+    // Method to create Task ID
+    public String createTaskID() {
+        // First two letters of task name
+        String firstTwoLetters = taskName.length() >= 2 ? taskName.substring(0, 2) : taskName;
+        // Last three letters of task developer
+        String lastThreeLetters = devDetails.length() >= 3 ? devDetails.substring(devDetails.length() - 3) : devDetails;
+        // Return task ID
+        return (firstTwoLetters + ":" + taskNum + ":" + lastThreeLetters).toUpperCase();
+    }
+
+    // Method to print task details
+    public String printTaskDetails() {
+        // Display task details
+        return "Task Status: " + taskStatus + "\n"
+                + "Developer Details: " + devDetails + "\n"
+                + "Task Number: " + taskNum + "\n"
+                + "Task Name: " + taskName + "\n"
+                + "Task Description: " + taskDescription + "\n"
+                + "Task ID: " + taskID + "\n"
+                + "Duration: " + taskDuration + " hours";
+    }
+
+    // Method to return task duration
+    public int returnTotalHours() {
+        // Return task duration
+        return this.taskDuration;
+    }
 }
